1676de26d5b0f1198e8a116e9a4b63bb06a4c0e4
omintbp <zhiltcov.ds@gmail.com>
2024-12-05 13:30:18 +1000

    F-1 Add tailwind and MUI

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+const Footer: FunctionComponent = () => {
+	return (
+		<>
+			<h2 className="text-3xl text-red-950">Footer</h2>
+		</>
+	);
+};
+
+export default Footer;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import "./index.css";
+import VolunteerPage from "./pages/VolunteersPage";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<Header />
+		<VolunteerPage />
+		<Footer />
+	</StrictMode>
+);
